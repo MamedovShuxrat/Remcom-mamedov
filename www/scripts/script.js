596b77511d96306394c06445bd55a2fd974de198
Shuxrat Mamedov <lider_20_10@mail.ru>
2022-02-04 07:44:06 +0300

    в катологе пробол сделать фильры , что-то пока не хочет работать ! стр с отзывами доверстал

--- www/scripts/script.js
+++ www/scripts/script.js
@@ -3,6 +3,30 @@ $(document).ready(function(){
     $('.js-nav-list').slideToggle();
 
     $('.js-nav-list').addClass('show');
-  })
+  });
+
+  // фильтры
+  $('.js-filter-link').on('click', function(event){
+    event.preventDefault();
+
+    const filterType = $(this).data('filter')
+
+    $('.js-filter-link').removeClass('active-1');
+    $(this).addClass('active-1');
+
+    if (filterType === 'all') {
+      $('.js-portfolio').show();
+      return;
+    }
+    $('.js-portfolio').each(function(){
+      const portfolioType = $(this).data('type');
+      if(filterType === portfolioType){
+        $(this).show();
+        return;
+      }
+
+      $(this).hide();
+    });
+  });
 
 })
